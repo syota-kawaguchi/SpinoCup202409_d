2e38ca466dd5907aecd3eac74cc7090247e8fcc9
kk0101010 <is0641si@ed.ritsumei.ac.jp>
2024-09-21 23:30:09 +0900

    マージでおかしくなったのでもっかいコミット

--- react/app/page.tsx
+++ react/app/page.tsx
@@ -69,9 +69,11 @@ export default function Home() {
     // model loader
     const fbxloader: FBXLoader = new FBXLoader();
     let mixer: THREE.AnimationMixer;
-    fbxloader.load("models/Tpose.fbx", (object) => {
-      object.position.set(0, 0, 0);
-      object.scale.set(0.05, 0.05, 0.05);
+    let loadedModel: THREE.Object3D; // Add this line
+    fbxloader.load("models/testbox.fbx", (object) => {
+      object.position.set(0, 0.8, 0);
+      object.scale.set(0.08, 0.05, 0.05);
+      loadedModel = object; // Add this line
       const anim = new FBXLoader();
       anim.load("models/CenterBlock.fbx", (anim) => {
         mixer = new THREE.AnimationMixer(object);
