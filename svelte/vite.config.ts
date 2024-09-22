cd92bdf69b92c869ce21c9941b02c18ca105900b
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-22 17:23:21 +0900

    不要なpah指定の削除

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: {
-		port: 3002
-	},
-	
 	plugins: [sveltekit()]
 });
--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -5,12 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3003
-  },
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
