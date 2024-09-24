dc0c80667ea6eb55c5ab624ee1004fdc0def86a2
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-24 12:43:55 +0900

    topページとtitleページを作成

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,39 +1,11 @@
-<script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+<!-- App.svelte -->
+<script>
+  import { Router, Route } from "svelte-routing";
+  import Top from "./pages/Top.svelte";
+  import Title from "./pages/Title.svelte";
 </script>
 
-<main>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Router>
+  <Route path="/svelte/" component={Top} />
+  <Route path="/svelte/title" component={Title} />
+</Router>
---
+++ svelte-app/src/pages/Top.svelte
@@ -0,0 +1,7 @@
+<!-- pages/Top.svelte -->
+<script>
+  export let name = "Svelte";
+</script>
+
+<h1>Top Page</h1>
+<p>This is the top page for {name}!</p>
