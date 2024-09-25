dc1774cc418b3f4014fe493dd5c42e6e0452b819
syota-kawaguchi <kawaguti0618@icloud.com>
2024-09-25 13:11:37 +0900

    ゲージの追加・不要なファイルの削除

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,9 +1,9 @@
 <!-- App.svelte -->
 <script lang="ts">
-  import SelectingCars from "./pages/SelectingCars.svelte";
-  import Title from  "./pages/Title.svelte";
-
-  let selectedCarID: number | null = null;
+  import { Canvas, useTask } from "@threlte/core";
+  import SelectingCarsComponets from "./components/SelectingCarsComponets.svelte";
+  import { carPaths } from "./const";
+    import CarParameter from "./components/CarParameter.svelte";
 
   const _cars = [
     { id: 1, name: "Car 1" },
@@ -12,7 +12,164 @@
   ];
 
   const _handleSelectCar = (id: number) => {
-    selectedCarID = id;
     localStorage.setItem("selectedCarID", id.toString());
   };
+
+  const intensityBright = 1.8
+  const intensityDart = 0.2
+
+  let selectingCarIndex = 0;
+  let selectingCarName  = _cars.find(car => car.id == selectingCarIndex + 1)?.name ?? ""
+  let selectedCarIndex = selectingCarIndex;
+  let rotateDirection = -1;
+  let onAnimation = false;
+  let intensity = intensityBright
+  const IncrementCarIndex = () => {
+    if (onAnimation) {
+      console.log("animation running")
+      return
+    }
+    OnAnimationStart()
+    if (selectingCarIndex < carPaths.length - 1) {
+      selectingCarIndex++
+    }
+    else {
+      selectingCarIndex = 0
+    }
+    rotateDirection = -1
+    onAnimation = true
+  }
+
+  const DecrementCarIndex = () => {
+    if (onAnimation) {
+      return
+    }
+    OnAnimationStart()
+    if (0 < selectingCarIndex) {
+      selectingCarIndex--
+    }
+    else {
+      selectingCarIndex = carPaths.length - 1
+    }
+    rotateDirection = 1
+    onAnimation = true
+  }
+
+  const Decide = () => {
+    _handleSelectCar(selectingCarIndex + 1)
+    //TODO(kawaguchi): go to next page
+    let item = localStorage.getItem("selectedCarID")
+    console.log(`localStorageItem : ${item}`)
+  }
+
+  const OnAnimationStart = () => {
+    selectedCarIndex = selectingCarIndex
+    intensity = intensityDart
+    selectingCarName = ""
+  }
+
+  function onAnimationFinished() {
+    selectingCarName = _cars.find(car => car.id == selectingCarIndex + 1)?.name ?? ""
+    onAnimation = false
+    intensity = intensityBright
+  }
+
 </script>
+
+<p class="selecting-car-name">
+  {selectingCarName}
+</p>
+
+<Canvas>
+  <SelectingCarsComponets 
+    selectedCarIndex={selectedCarIndex} 
+    selectingCarIndex={selectingCarIndex} 
+    rotateDirection={rotateDirection}
+    onAnimationFinished={onAnimationFinished}
+    intensity={intensity}
+  />
+</Canvas>
+
+<button class="next-car-button" on:click={IncrementCarIndex}>
+  次へ
+</button>
+
+<button class="prev-car-button" on:click={DecrementCarIndex}>
+  戻る
+</button>
+
+<div class="container">
+  <CarParameter label={"Hot"} maxGuage={5} guage={2}/>
+  <CarParameter label={"Hot"} maxGuage={5} guage={3}/>
+  <button class="done-button" on:click={Decide}>
+    決定
+  </button>
+</div>
+
+<style>
+  .selecting-car-name {
+    margin: 0 auto;
+    padding: 0;
+    position: absolute;
+    top: 10%;
+    right: 0;
+    left: 0;
+    width: 300px;
+    height: 80px;
+    font-size: 40px;
+    z-index: 1;
+    /* display: block; */
+    color: white;
+    text-align: center;
+  }
+
+  .next-car-button{
+    padding: 0;
+    position: absolute;
+    bottom: 50%;
+    right: 10%;
+    width: 80px;
+    height: 80px;
+    z-index: 1;
+    /* display: block; */
+    color: white;
+    background-color: blue;
+    text-align: center;
+  }
+
+  .prev-car-button{
+    padding: 0;
+    position: absolute;
+    bottom: 50%;
+    left: 10%;
+    width: 80px;
+    height: 80px;
+    z-index: 1;
+    /* display: block; */
+    color: white;
+    background-color: blue;
+    text-align: center;
+  }
+
+  .container {
+    margin: 0 auto;
+    padding: 0;
+    position: absolute;
+    bottom: 12%;
+    right: 0;
+    left: 0;
+    text-align: center;
+    z-index: 1;
+  }
+
+  .done-button{
+    margin: 0 auto;
+    padding: 0;
+    width: 200px;
+    height: 80px;
+    z-index: 1;
+    color: white;
+    background-color: blue;
+    text-align: center;
+  }
+</style>
--- svelte-app/src/components/SelectingCarsComponets.svelte
+++ svelte-app/src/components/SelectingCarsComponets.svelte
@@ -2,10 +2,12 @@
   import { T, useTask } from '@threlte/core'
   import { interactivity, GLTF, Environment } from '@threlte/extras'
   import { carPaths } from "../const";
+    import { Mesh } from 'three';
 
   export let selectedCarIndex: number
   export let selectingCarIndex: number
   export let rotateDirection: number  //時計回り : -1, 反時計回り : 1
+  export let intensity: number
   export let onAnimationFinished: () => void
 
   // 縁を描くように車を等間隔に配置する関数
@@ -27,17 +29,18 @@
   interactivity()
 
   const radius = 5 //車を円形に配置する半径
+  const rotationSpeedRate = 2 //回転する速さの倍率
   const lightPosition : number[] = [0, 10, 10]
   const cameraPosition : number[] = [4, 4, 10]
-  const carsCenterPosition : number[] = [1, 2.3, -3]//車を円形に配置する円心
-  const circlePosition : number[] = [0, 1, 0] //車の下に表示されている円のポジション
+  const carsCenterPosition : number[] = [1, 2.8, -3]//車を円形に配置する円心
+  const circlePosition : number[] = [0, 1.5, 0] //車の下に表示されている円のポジション
 
   let carRotation = CalcCarLookAt(carPaths.length, selectedCarIndex)
   let carRotationAfterChanged = CalcCarLookAt(carPaths.length, selectingCarIndex)
   useTask((delta) => {
     if (selectedCarIndex != selectingCarIndex) {
       const radian = (360 / carPaths.length) * (Math.PI / 180)
-      carRotation += (delta * rotateDirection)
+      carRotation += (delta * rotateDirection * rotationSpeedRate)
       if (radian <= Math.abs(carRotation - carRotationAfterChanged)) {
         selectedCarIndex = selectingCarIndex
         carRotationAfterChanged = carRotation
@@ -57,13 +60,9 @@
 />
 
 <T.DirectionalLight 
+  intensity={intensity}
   position={lightPosition} 
   castShadow
-/>
-
-<Environment
-	files={'https://static8.depositphotos.com/1035653/917/i/450/depositphotos_9173686-stock-photo-empty-auto-repair-shop-for.jpg'}
-	isBackground={true}
 />
 
 <T.Mesh
@@ -71,16 +70,29 @@
   rotation.y={-carRotation}
 >
   {#each { length: carPaths.length } as _, i}
-    <T.Mesh
-      position={ArrangeCarsCircle(radius, carPaths.length, i)}
-      rotation.y={carRotation}
-    >
-      <GLTF url={carPaths[i]}/>
-    </T.Mesh>
+    {#if -1 < carPaths[i].indexOf("car03")}
+      <!-- 大きな車の表示位置がズレるので個別で修正 -->
+      <T.Mesh
+        position={ArrangeCarsCircle(radius, carPaths.length, i)}
+        rotation.y={carRotation}
+      >
+        <T.Mesh position={[-0.5, 0, -2]}>
+          <GLTF url={carPaths[i]}/>
+        </T.Mesh>
+      </T.Mesh>
+    {:else}
+      <T.Mesh
+        position={ArrangeCarsCircle(radius, carPaths.length, i)}
+        rotation.y={carRotation}
+      >
+        <GLTF url={carPaths[i]}/>
+      </T.Mesh>
+    {/if}
   {/each}
 </T.Mesh>
 
 <T.Mesh
+  enable={intensity != 0}
   position={circlePosition}
   rotation.x={-Math.PI / 2}
   receiveShadow
