82ca4a5e30b836535aa7793d2481ac6fbc2f6382
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-26 04:36:14 +0900

    変えちゃいけなかったとこ戻す

--- react-app/src/const.ts
+++ react-app/src/const.ts
@@ -1,5 +1,8 @@
-export const niku: number = 150;
-export const tamanegi: number = 100;
-export const medamayaki: number = 50;
+export const niku: number = 1500;
+export const tamanegi: number = 1000;
+export const medamayaki: number = 500;
+
+export const foodScore: number[] = [30,20,10];
+
 export const timeMax: number = 100;
-export const carSizes: number[][] = [[26,12],[38,14],[60,36]];
+export const carSizes: number[][] = [[26,12,-1.5,1],[38,14,0,1.5],[60,36,5,1.5]];
