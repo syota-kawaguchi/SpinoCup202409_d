0406c887042ce462bc4506297393d686ad069f56
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-26 08:36:39 +0900

    加点時に加点分のスコアを取得

--- react-app/src/component/DisplayCurrentScore.tsx
+++ react-app/src/component/DisplayCurrentScore.tsx
@@ -3,6 +3,11 @@ import styles from "./displayCurrentScore.module.css";
 import { Manager } from "../class";
 import { useEffect, useState } from "react";
 
+interface AddScore {
+  additionalScore: number;
+  time: number;
+}
+
 export const DisplayCurrentScore = ({
   managerObj,
 }: {
@@ -10,13 +15,27 @@ export const DisplayCurrentScore = ({
 }) => {
   // Reactでstateとしてscoreを保持する
   const [currentScore, setCurrentScore] = useState(managerObj.score);
+  //   加点があった場合に加点分を下に表示するためのstate
+  const [addScore, setAddScore] = useState<AddScore[]>([]);
+  console.log(addScore);
 
   useEffect(() => {
     // スコアが更新されたかどうかを監視するためにintervalを設定
     const interval = setInterval(() => {
+      const newAddScore = addScore.filter((score) => score.time < 1); // 1秒経過した加点分を削除
+
       if (managerObj.score !== currentScore) {
         setCurrentScore(managerObj.score); // stateに新しいスコアを反映
+        newAddScore.push({
+          additionalScore: managerObj.score - currentScore,
+          time: 0,
+        });
       }
+      newAddScore.forEach((score) => {
+        score.time += 0.1; // 加点分の表示時間を更新
+      });
+
+      setAddScore([...newAddScore]); // 加点分をstateに追加
     }, 100); // 100msごとにチェック (必要に応じて調整)
 
     return () => clearInterval(interval); // コンポーネントのアンマウント時にクリーンアップ
@@ -25,6 +44,13 @@ export const DisplayCurrentScore = ({
   return (
     <div className={styles.container}>
       <p className={styles.score}>{currentScore}</p>
+      <div className={styles.addScore}>
+        {addScore.map((score, index) => (
+          <p key={index} className={styles.addScoreText}>
+            {score.additionalScore}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
